7720586dddb57d1e8116aa385bc2f5edd99ba6d6
rajibcegisbd <rbhowmikcse@gmail.com>
2023-05-30 21:42:55 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitTestRBA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
